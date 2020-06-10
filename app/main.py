c528b35abd1deb87a62c331875655b336131c5ab
gyh1621 <cngyh96@gmail.com>
2020-06-10 15:55:46 +0000

    add bright style

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 
 import json
 from urllib.parse import unquote
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 app = Flask(__name__)
 
 from mercury_parser.client import MercuryParser
@@ -17,9 +17,16 @@ def index():
 @app.route('/parse', methods=['GET'])
 def parse():
     url = unquote(request.args.get('url'))
+    style = request.args.get('style')
     result = parser.parse_article(url).json()
+    if style == 'dark':
+        css = url_for('static', filename='dark.css')
+    else:
+        css = url_for('static', filename='bright.css')
     return render_template('article.html',
                            title=result['title'],
                            content_title=result['title'],
-                           content=result["content"])
+                           content=result["content"],
+                           css=css
+                           )
 
