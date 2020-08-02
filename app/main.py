6d797c06733c10ba37980de2fb1da87f990bd1d0
gyh1621 <cngyh96@gmail.com>
2020-08-02 08:20:32 -0400

    pass original url

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 # coding: utf-8
 
+import re
 import json
 from urllib.parse import unquote
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 app = Flask(__name__)
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 from mercury_parser.client import MercuryParser
 
